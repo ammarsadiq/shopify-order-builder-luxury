815fa94f6446c01cf7d4f0fcc4f5d6ea3f68fe9c
Ammar sadiq <ammarsadiq450@gmail.com>
2026-09-14 15:22:50 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 
+export default {
+  async fetch(request, env) {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Shopify Order Builder API is running"
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+};
